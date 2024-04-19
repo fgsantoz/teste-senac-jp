57c9640a3281ff16687f1a9be2f15b17ff775b84
Fabricio Santos <fgsantoz@outlook.com>
2024-04-19 10:45:12 -0300

    ex-028 ok

--- ex-028/ex-028.cpp
+++ ex-028/ex-028.cpp
@@ -1,4 +1,4 @@
-// 28. FUP que leia 10 números e verifique quantos destes números são negativos;;
+// 28. FUP que leia 10 números e verifique quantos destes números são negativos;
 
 #include <iostream>
 
@@ -6,11 +6,27 @@ using namespace std;
 
 main() {
 	
-		system("chcp 65001");
-
+	system("chcp 65001");
 	
-    
-    
-    	    
+	const int arytmn = 10;
+	int vlr[arytmn], ctmnos = 0;
+	
+	cout <<"\n \t Informe os números... \n";
+	
+	for (int i = 0; i < arytmn; ++i) {
+		
+		cout <<"\n \t\t Número " <<i + 1 <<": ";
+		cin >>vlr[i];
+		
+		if(vlr[i] < 0) {
+			
+			ctmnos++;
+			
+		}
+			
+	}
+	
+	cout <<"\n \t O total de valores negativos é " <<ctmnos <<"\n\n";     
+      
 }
 
