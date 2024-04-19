c8906ecb47baf2bcb7047d1860a3e6cd990b4549
Fabricio Santos <fgsantoz@outlook.com>
2024-04-18 22:45:51 -0300

    organizando o código

--- teste-pt-001/estruturaProva.cpp
+++ teste-pt-001/estruturaProva.cpp
@@ -6,27 +6,33 @@
 using namespace std;
 
 main() {
-    int op;
+
     system("chcp 65001");
     
-    double cels, fahr; // Q-04
-    
-    
+    int op;
+    double cels, fahr;
+    char gen;
+	float alt, imc;
+    // float pes;  
     
     do{
-        cout<<"\n \t Selecione uma opção no menu abaixo: \n";
-        cout<<"\n [1] Exercício 04";
-        cout<<"\n [2] Exercício 10";
-        cout<<"\n [3] Exercício 17";
-        cout<<"\n [4] Exercício 24";
-        cout<<"\n [5] Exercício 28";
-        cout<<"\n [6] Use a sua criatividade";
-        cout<<"\n [7] Encerrar \n\n";
-        cout<<"\n Digite a opção: ";
-        cin>>op;
+
+        cout <<"\n \t Selecione uma opção no menu abaixo: \n";
+        cout <<"\n [1] Exercício 04";
+        cout <<"\n [2] Exercício 10";
+        cout <<"\n [3] Exercício 17";
+        cout <<"\n [4] Exercício 24";
+        cout <<"\n [5] Exercício 28";
+        cout <<"\n [6] Use a sua criatividade";
+        cout <<"\n [7] Encerrar \n\n";
+        cout <<"\n Digite a opção: ";
+        cin >>op;
+
         switch(op) {
+
             case 1: {
-                cout<<"\n Conversor de Temperatura: \n";
+                
+                cout <<"\n Conversor de Temperatura: \n";
                 
                 cout <<"\n Digite a temperatura em Celsius: ";
     			cin >> cels;
@@ -36,36 +42,86 @@ main() {
    				cout <<"\n A temperatura em Fafrenheit é " <<fahr <<"ºF\n";                            
                                 
                 break;
+
             };
+
             case 2: {
-                cout<<"Sendo criativo";
+                
+                cout <<"Calculadora de IMC: \n";
+
+                cout <<"\n Informe seu gênero... \n\n (M: para masculino | F: para feminino): ";
+                cin >>gen;
+        
+                // cout <<"\n Informe seu peso (em quilogramas): ";
+                // cin >>pes; 
+                // devido a formula dada no enunciado, a solicitação de peso, por hora, se tornou dispensável e, portanto, foi desativada do código;
+    
+                cout <<"\n Informe sua altura (em metros): ";
+                cin >>alt;    
+    
+                if(gen == 'M' || gen == 'm') {
+            
+                    imc = (72.7*alt)-58;
+                    cout <<"\n Seu IMC é " <<imc <<"\n\n";
+
+                }
+    	
+                else if(gen == 'F' || gen == 'f') {
+            
+                    imc = (62.1*alt)-44.7;
+                    cout <<"\n Seu IMC é " <<imc <<"\n\n";
+
+                }
+
                 break;
+
             };
+
             case 3: {
-                cout<<"Sendo criativo";
+
+                cout <<"Sendo criativo";
                 break;
+
             };
+
             case 4: {
-                cout<<"Sendo criativo";
+
+                cout <<"Sendo criativo";
                 break;
+
             };
+
             case 5: {
-                cout<<"Sendo criativo";
+
+                cout <<"Sendo criativo";
                 break;
+
             };
+
             case 6: {
-                cout<<"Sendo criativo";
+
+                cout <<"Sendo criativo";
                 break;
+
             };
+
             case 7: {
-                cout<<"Finalizando";
+
+                cout <<"Finalizando";
                 break;
+
             };
+
             default:
-                cout<<"\nOpção inválida";
+
+                cout <<"\nOpção inválida";
                 break;
+
         }
+
     }while(op != 7);
-    cout<<"\n\n\n\n\n - Até mais \n\n\n\n\n";
+    
+    cout <<"\n\n\n\n\n - Até mais \n\n\n\n\n";
+    
 }
 
